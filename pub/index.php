6fdee8f30458accd9bc1c6dd610da67664a328f0
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2019-12-19 23:03:37 +0100

    add moving to js add ids to squares add id to game

--- pub/index.php
+++ pub/index.php
@@ -9,7 +9,11 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER['REQUEST_URI'])) {
 }
 
 if (strpos($_SERVER['REQUEST_URI'], '/api/') === 0) {
-    require ApiRouter();
+//    $api = new Api();
+//    $api->sendResponse();
+
+    echo '["\ud83d\udc34","\ud83e\udddd","\ud83e\udd34","\ud83d\udc78","\ud83e\udddd","\ud83d\udc34","\ud83d\uddfc","\ud83d\udc82","\ud83d\udc82","\ud83d\udc82","\ud83d\udc82","\ud83d\udc82","\ud83d\udc82","\ud83d\udc82","\ud83d\udc82","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\u2b1b","\u2b1c","\ud83d\udc6e","\ud83d\udc6e","\ud83d\udc6e","\ud83d\udc6e","\ud83d\udc6e","\ud83d\udc6e","\ud83d\udc6e","\ud83d\udc6e","\ud83e\udda5","\ud83d\uddfc","\ud83c\udff0","\ud83c\udfc3","\ud83e\udd35","\ud83d\udc70","\ud83c\udfc3","\ud83e\udda5","\ud83c\udff0"]';
+
 
     return;
 }
@@ -28,4 +32,4 @@ if ($_SERVER['REQUEST_URI'] === '/board') {
 }
 
 http_response_code(404);
-echo '<h1>404 - Not fouund</h1>';
+echo '<h1>404 - Not found</h1>';
