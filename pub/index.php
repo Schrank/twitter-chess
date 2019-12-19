5daf73752c41833af727304539c78a234d4bf9e8
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2019-12-19 09:38:19 +0100

    render board with JS have base.phtml template

--- pub/index.php
+++ pub/index.php
@@ -8,5 +8,24 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER['REQUEST_URI'])) {
     return false; // Liefere die angefragte Ressource direkt aus
 }
 
-require '../templates/board.phtml';
+if (strpos($_SERVER['REQUEST_URI'], '/api/') === 0) {
+    require ApiRouter();
 
+    return;
+}
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    $page = 'login';
+    require '../templates/base.phtml';
+
+    return;
+}
+if ($_SERVER['REQUEST_URI'] === '/board') {
+    $page = 'board';
+    require '../templates/base.phtml';
+
+    return;
+}
+
+http_response_code(404);
+echo '<h1>404 - Not fouund</h1>';
